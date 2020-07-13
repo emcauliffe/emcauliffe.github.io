facee17e7659d438beba615985611f1b38c0b9b7
emcauliffe <14607273+emcauliffe@users.noreply.github.com>
2020-07-12 21:22:01 -0400

    Progress bar on blog posts

--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -42,13 +42,15 @@ export default function BlogPost(props) {
 			.catch(error => console.log(error))
 	}, [file])
 
+	const target = React.createRef();
 
 	return (
 		file === 404 ? <Redirect to="/404" /> :
 			<div>
 				<Header heading="Ethan McAuliffe's Blog" link="/blog" />
 				<PostTitle title={frontMatter.title} date={frontMatter.date} image={frontMatter["feature-img"]} />
-				<div>
+				<ReadingProgress target={target} />
+				<div className={`post`} ref={target}>
 					<ReactMarkdown
 						className="post-body"
 						source={text}
@@ -64,3 +66,35 @@ export default function BlogPost(props) {
 			</div>
 	)
 }
+
+function ReadingProgress({ target }) {
+
+	const [readingProgress, setReadingProgress] = useState(0);
+
+	function scrollListener() {
+		if (!target.current) {
+			return;
+		}
+
+		const element = target.current;
+		const totalHeight = element.clientHeight - element.offsetTop;
+		const windowScrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+		if (windowScrollTop === 0) {
+			return setReadingProgress(0);
+		}
+
+		if (windowScrollTop > totalHeight) {
+			return setReadingProgress(100);
+		}
+
+		setReadingProgress((windowScrollTop / totalHeight) * 100);
+	}
+
+	useEffect(() => {
+		window.addEventListener("scroll", scrollListener);
+		return () => window.removeEventListener("scroll", scrollListener);
+	})
+
+	return <div className={`reading-progress-bar`} style={{ width: `${readingProgress}%` }} />
+};
