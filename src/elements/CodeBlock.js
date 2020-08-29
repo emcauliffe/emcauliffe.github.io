cc8c8fa7f61a2996dcf0b7da141a876422877495
emcauliffe <14607273+emcauliffe@users.noreply.github.com>
2020-08-29 19:03:36 -0400

    Code for a future night mode

--- src/elements/CodeBlock.js
+++ src/elements/CodeBlock.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { github } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+// eslint-disable-next-line
+import { github, dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export default function CodeBlock(props) {
 
 	return (
 		<SyntaxHighlighter language={props.language} style={github} showLineNumbers={true} customStyle={{ fontSize: 13 }}>
+		{/* <SyntaxHighlighter language={props.language} style={dracula} showLineNumbers={true} customStyle={{ fontSize: 13 }}> */}
 			{props.value}
 		</SyntaxHighlighter>
 	)
