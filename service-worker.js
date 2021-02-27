df0eb027e8422e05ccb1fd463465f7b0afed1742
emcauliffe <14607273+emcauliffe@users.noreply.github.com>
2021-02-27 17:12:07 +0000

    Deploying to master from  @ dd126ed53846dca1ce7caa62aac1863bcb17639d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ff4c8e96adf58d60c8448ef84390a7d.js"
+  "/precache-manifest.15e8fa9736c6e98db4efe44fc83749f5.js"
 );
 
 self.addEventListener('message', (event) => {
